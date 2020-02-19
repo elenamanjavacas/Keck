5c2ac9be8308d84015f4b9f5e88c6192f393937a
elenamanjavacas <54870952+elenamanjavacas@users.noreply.github.com>
2020-02-19 11:37:41 -1000

    Update open_csu_slits.py

--- open_csu_slits.py
+++ open_csu_slits.py
@@ -9,22 +9,27 @@
 #python_version  :2.6.6
 #==============================================================================
 # importing needed packages
-from .core import *
-from .mask import *
-from .mechs import *
-from .detector import *
-from .csu import *
-from .analysis import *
+#from .core import *
+#from .mask import *
+#from .mechs import *
+#from .detector import *
+#import csu
+from core import *
+from csu import *
+#from .analysis import *
 import os
 from decimal import Decimal
 import subprocess
+import time
 #==============================================================================
 
 def open_csu_slits(slitwidth=0.7):
 
 # Unless otherwise defined, we assume that the slitwidht is 0.7"
 
-    slitwidth_mm = (slitwidth+0.2)*0.7256
+    #slitwidth_mm = (slitwidth+0.2)*0.7256
+    slitwidth_mm = 0.65
+    print(slitwidth_mm)
     print("Open alignment boxes to 9-10 arcsec")
 
 # We determine the position of the odd and even CSU bars, and their difference, to determine the width of the slits:
@@ -48,68 +53,73 @@ for i in range(1,47):
     #pos_odd = float(odd_bar[33:42])
 
     odd_bar = get(f"B{2*i-1:02d}POS", 'mcsus', mode=float)
-    print('Position odd bar: ',pos_odd)
+    print('Position odd bar: ',odd_bar)
 
     # Difference of both positions, to determine the width of the all the slits:
 
-    Diff_Bars = abs(pos_even-pos_odd)
-    print('Difference between bars ' + "{0:0=2d}".format(2*i+1) +' and ' + "{0:0=2d}".format(2*i) + ' in mm :' , Diff_Bars)
+    Diff_Bars = abs(even_bar-odd_bar)
+    print('Difference between bars ' + "{0:0=2d}".format(2*i-1) +' and ' + "{0:0=2d}".format(2*i) + ' in mm :' , Diff_Bars)
 
     # Open the bars if the difference beteween the bars is bigger than 3 arcsec (2.1768 mm), and smaller than 354.0 arcsec (250.33 mm):
 
-    if Diff_Bars > slitwidth_mm and Diff_Bars < 250.33:
+    if Diff_Bars > 0.65 and Diff_Bars < 250.33:
         print('Open the alingment boxes!')
         print(' ')
 
         # Open odd bars by 2.5 arcseconds (1.814mm)
-        new_pos_odd = pos_odd - 1.814
+        new_pos_odd = odd_bar - 1.814
         print('Opening odd bars 2.5 arcsec')
         #subprocess.Popen('modify -s mcsus '+'B'+"{0:0=2d}".format(2*i-1)+'TARG = ' + str(new_pos_odd),shell=True, stdout=subprocess.PIPE).stdout
         set('B'+"{0:0=2d}".format(2*i-1)+'TARG',str(new_pos_odd),service='mcsus', wait=True)
 
         # Open even bars by 2.5 arcseconds (1.814mm)
-        new_pos_even = pos_even+1.814
+        new_pos_even = even_bar+1.814
         print('Opening even bars 2.5 arcsec')
         #subprocess.Popen('modify -s mcsus '+'B'+"{0:0=2d}".format(2*i)+'TARG = ' + str(new_pos_even),shell=True, stdout=subprocess.PIPE).stdout
         set('B'+"{0:0=2d}".format(2*i)+'TARG',str(new_pos_even),service='mcsus', wait=True)
 
-        # Setup the name of the new mask:
-        #mask_name0 = subprocess.Popen('show -s mcsus -terse maskname',shell=True, stdout=subprocess.PIPE).stdout
-        mask_name0 = get('MASKNAME',service='mcsus',mode=str)
-        mask_name = mask_name0.read().rstrip().replace(" (align)", "")
 
-
-        print('Current mask',mask_name)
-        #current_mask = subprocess.Popen('modify -s mcsus SETUPNAME=' + mask_name + '_wide', shell=True, stdout=subprocess.PIPE).stdout
-        current_mask = set('SETUPNAME',VALUE = mask_name + '_wide', service='mcsus')
-
-
-        # Setup the CSU mask with wider alignment boxes
-        print('Setup the new alignment mask with wider alignment boxes')
-        #subprocess.Popen('modify -s mosfire csusetup=1', shell=True, stdout=subprocess.PIPE).stdout
-        set('CSUSETUP', 1)
-        print('Setting up CSU')
-
-        # Check that the status of the CSU is set up:
-
-        #csu_setup0 = subprocess.Popen('show -s mcsus CSUREADY',shell=True, stdout=subprocess.PIPE).stdout
-        #csu_setup_state = csu_setup0.read()
-        csu_setup_state=get('CSUREADY', service='mcsus',mode=float)
-        print('CSU state: ',csu_setup_state)
-
-
-        # If the CSU is setup (= 2), then open the alignment boxes 5 arcsec:
-
-        if csu_setup_state = 2: #(checkout that this state is =2)
-
-        print('Open the alignment boxes 5 arcsec')
-        #subprocess.Popen('modify -s mosfire csugo=1', shell=True, stdout=subprocess.PIPE).stdout
-        execute_mask()
-        waitfor_CSU()
-
-        print('Alignment boxes have been open by 5 arcsec, take an image to see if you find your alignment stars')
 
 
     else:
-        print('You are not allowed to open the bars anymore!')
+        print('You are not allowed to open the bars anymore or you got a slit there!')
         print(' ')
+
+# Setup the name of the new mask:
+#mask_name0 = subprocess.Popen('show -s mcsus -terse maskname',shell=True, stdout=subprocess.PIPE).stdout
+mask_name0 = get('MASKNAME',service='mcsus',mode=str)
+print(mask_name0)
+mask_name = mask_name0.rstrip().replace(" (align)", "")
+
+
+print('Current mask',mask_name)
+#current_mask = subprocess.Popen('modify -s mcsus SETUPNAME=' + mask_name + '_wide', shell=True, stdout=subprocess.PIPE).stdout
+current_mask = set('SETUPNAME',mask_name + '_wide', service='mcsus')
+
+
+# Setup the CSU mask with wider alignment boxes
+print('Setup the new alignment mask with wider alignment boxes')
+#subprocess.Popen('modify -s mosfire csusetup=1', shell=True, stdout=subprocess.PIPE).stdout
+set('CSUSETUP', 1)
+print('Setting up CSU')
+
+# Check that the status of the CSU is set up:
+
+#csu_setup0 = subprocess.Popen('show -s mcsus CSUREADY',shell=True, stdout=subprocess.PIPE).stdout
+#csu_setup_state = csu_setup0.read()
+csu_setup_state=get('CSUREADY', service='mcsus',mode=float)
+print('CSU state: ',csu_setup_state)
+
+# If the CSU is setup (= 2), then open the alignment boxes 5 arcsec:
+waitfor_CSU()
+csu_setup_state=get('CSUREADY', service='mcsus',mode=float)
+print('CSU state: ',csu_setup_state)
+
+if csu_setup_state==2: #(checkout that this state is =2)
+
+    print('Open the alignment boxes 5 arcsec')
+#subprocess.Popen('modify -s mosfire csugo=1', shell=True, stdout=subprocess.PIPE).stdout
+    execute_mask()
+    waitfor_CSU()
+
+    print('Alignment boxes have been open by 5 arcsec, take an image to see if you find your alignment stars')
